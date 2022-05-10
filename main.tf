9273e7b10cee2d1d3a27c644a48a9d1d61817e84
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2022-05-10 15:16:35 +0530

    Update main.tf
    update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,13 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "test" {
+  bucket = "my-jatinbamel-test-terraform"
 
-  tags = var.tags
-}
-
-resource "aws_s3_bucket_website_configuration" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          aws_s3_bucket.s3_bucket.arn,
-          "${aws_s3_bucket.s3_bucket.arn}/*",
-        ]
-      },
-    ]
-  })
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.test.id
+  acl    = "private"
 }
