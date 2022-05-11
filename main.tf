0e59147d79b98ff40ba8035b9460428d074bcac9
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2022-05-11 16:25:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
 terraform {
-# Terraform version at the time of writing this post
-  required_version = ">= 0.12.24"
-
   backend "s3" {
     bucket = "awsnow.in"
-    key = "awsnow.in/terraform/backend.tfstate"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
   }
-
 }
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-provider "random" {}
