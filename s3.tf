7630ce0fec56140494f061d28ae5129e74bb6975
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2022-05-12 12:36:16 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
