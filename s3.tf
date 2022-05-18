57bbd829a69816cd179729d9f52ba89da2b3b16c
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2022-05-18 17:33:11 +0530

    apply server side encryption

--- s3.tf
+++ s3.tf
@@ -11,3 +11,13 @@ resource "aws_s3_bucket_acl" "s3-bucket" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
