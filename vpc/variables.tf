fa6f2cce8ca827f8ffd59997c959779587bc7388
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2022-05-20 12:04:11 +0530

    Create variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,34 @@
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the vpc"
+  type        = string
+  default     = "172.16.0.0/16"
+}
+variable "tags" {
+  type        = map(any)
+  description = "List of tags to be added to the resources"
+  default     = {
+    project     = "test",
+    environment = "dev"
+}
+
+variable "account_no" {
+  type = number
+}
+
+variable "region_code" {
+  description = "The short code for region used for naming resources"
+}
+
+variable "region" {
+  description = "The region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "availability_zones" {
+  type        = list(any)
+  description = "The az that the resources will be launched"
+  default     = []
+}
+
